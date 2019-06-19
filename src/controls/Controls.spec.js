dc641310865e43d774e6ba898a629388ba3d85a0
Seth Lachman <lachmanseth17@gmail.com>
2019-06-19 17:19:04 -0400

    Added tests for disabled property in control component.

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
+import "jest-dom/extend-expect";
 import renderer from "react-test-renderer";
 import "@testing-library/react/cleanup-after-each";
 
@@ -23,5 +24,31 @@ describe("<Controls />", () => {
     const button = getByText(/close gate/i);
     fireEvent.click(button);
     getByText(/open gate/i);
+  });
+
+  // open gate to close gate
+  it("should toggle text of open gate to close gate", () => {
+    const { getByText } = render(<Dashboard />);
+    const closeSwitch = getByText(/close gate/i);
+    fireEvent.click(closeSwitch);
+
+    const openSwitch = getByText(/open gate/i);
+    fireEvent.click(openSwitch);
+    getByText(/close gate/i);
+  });
+
+  it("should have disabled property on lock gate when gate is unlocked", () => {
+    const { getByText } = render(<Dashboard />);
+    const button = getByText(/lock gate/i);
+    expect(button).toBeDisabled();
+  });
+
+  it("should have disabled property on open gate when gate is locked", () => {
+    const { getByText } = render(<Dashboard />);
+    let openControl = getByText(/close gate/i);
+    fireEvent.click(openControl);
+    fireEvent.click(getByText(/lock gate/i));
+
+    expect(openControl).toBeDisabled();
   });
 });
