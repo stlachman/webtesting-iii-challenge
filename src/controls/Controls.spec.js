43830656d1aeddc9828ccbab75851f24cd58dbb8
Seth Lachman <lachmanseth17@gmail.com>
2019-06-19 16:59:23 -0400

    Added tests to control component.

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,13 +1,27 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import renderer from "react-test-renderer";
 import "@testing-library/react/cleanup-after-each";
 
+import Dashboard from "../dashboard/Dashboard";
 import Controls from "./Controls";
 
 describe("<Controls />", () => {
   it("matches snapshot", () => {
     const control = renderer.create(<Controls />).toJSON();
     expect(control).toMatchSnapshot();
+  });
+
+  it("should have a close gate and lock gate button", () => {
+    const { getByText } = render(<Controls />);
+    getByText(/close gate/i);
+    getByText(/lock gate/i);
+  });
+
+  it("should toggle text of close to open", () => {
+    const { getByText } = render(<Dashboard />);
+    const button = getByText(/close gate/i);
+    fireEvent.click(button);
+    getByText(/open gate/i);
   });
 });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
-// Test away!
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/react/cleanup-after-each";
+
+import Display from "./Display";
+
+describe("<Display />", () => {
+  it("close gate click toggles open to closed", () => {
+    const { getByText } = render(<Display />);
+  });
+});
