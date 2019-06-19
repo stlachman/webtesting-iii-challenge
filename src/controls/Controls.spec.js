fd31236ebe3cb14591b95be69760491e769def5b
Seth Lachman <lachmanseth17@gmail.com>
2019-06-19 15:46:36 -0400

    Added snapshot test to controls component.

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,13 @@
-// Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import "@testing-library/react/cleanup-after-each";
+
+import Controls from "./Controls";
+
+describe("<Controls />", () => {
+  it("matches snapshot", () => {
+    const control = renderer.create(<Controls />).toJSON();
+    expect(control).toMatchSnapshot();
+  });
+});
